46838f74edd119bcf795b62da3699d685c9d3697
lunalagosusm <33042856+lunalagosusm@users.noreply.github.com>
2018-08-19 18:29:49 -0300

    commit inicial
    commit inicial

---
+++ ospf.h
@@ -0,0 +1,14 @@
+#ifndef OSPF_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void generaRuta(char * rutaTop, char * salidaRut);
+void ospf(int src_router, int nodos, int cost_matrix[nodos][nodos],
+		  int links_matrix[nodos][nodos], FILE *archivo_rut);
+int buscanodos(char rutaarchivo[100]);
+#endif
+
+
