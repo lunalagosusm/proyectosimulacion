52c62c058af14bab7fd115885f60cd0fb3f0386c
simisterio <simon.rv92@gmail.com>
2018-08-20 11:12:28 -0700

    ahi va todo

--- ospf.c
+++ ospf.c
@@ -18,7 +18,7 @@ void generaRuta(char *rutaTop, char *salidaRut){
     int links_matrix[nodos][nodos];
     archivo = fopen(rutaTop,"r");
     archivo_rut = fopen(salidaRut,"w");
-    archivo_top = fopen("../proyectosimulacion/ArpaNet_new.top","w");
+    archivo_top = fopen("../proyectosimulacion/Red_new.top","w");
 
     //Se inicia la matriz de pesos
     for(i = 0; i < nodos; i++){
